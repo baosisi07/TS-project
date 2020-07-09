10ae5166ef97eebd710378f00b2e03f63916c458
baoss <18018021075@163.com>
2020-07-09 18:12:38 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+var message:string = "Hello World" 
+console.log(message)
+var tuple = [10, '123'];
+tuple[0] = 20;
+console.log(tuple)
+
+var num:number|string;
+num = 12;
+console.log(num)
+num = "123456";
+console.log(num)
+document.querySelectorAll(".app")[0].innerHTML = message;
