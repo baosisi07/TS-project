c436fe3f344f8e273241e53923d95bd5c04aeb33
baoss <18018021075@163.com>
2020-07-15 18:32:43 +0800

    类型保护函数

---
+++ src/advantage.ts
@@ -0,0 +1,35 @@
+// 类型保护
+
+interface Aa {
+    eat(): {};
+    fly(): {}
+}
+
+interface Bb {
+    eat(): {};
+    say(): {}
+}
+
+function protA(arg:Aa | Bb) {
+    arg.eat() // 可直接访问公共属性
+    if((<Aa>arg).fly) { // 访问其他方法需要加类型断言
+        (<Aa>arg).fly()
+    } else {
+        (<Bb>arg).say()
+    }
+}
+
+// 类型保护函数
+function isAa(arg: Aa | Bb): arg is Aa { // arg is Aa 是类型谓词
+        return (<Aa>arg).fly !== undefined
+    }
+
+function protB(arg: Aa | Bb) {
+    arg.eat()
+    if (isAa(arg)) {
+        arg.fly
+    } else {
+        arg.say
+    }
+}
+
